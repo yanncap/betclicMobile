33ed36c92fdedddc928bb01817a042c9e39d87dd
schiff <idschiff@gmail.com>
2017-03-16 14:30:23 +0100

    maj authentification

--- app/src/main/java/betclic/m2i/com/betclicmobile/api/BetclicApi.java
+++ app/src/main/java/betclic/m2i/com/betclicmobile/api/BetclicApi.java
@@ -6,6 +6,7 @@ import betclic.m2i.com.betclicmobile.models.Meeting;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 /**
  * Created by formation on 16/03/17.
@@ -16,5 +17,5 @@ public interface BetclicApi {
     public Call<List<Meeting>> getMeetings();
 
     @POST("api/login")
-    public Call<String> login(String email, String password);
+    public Call<String> login(@Query("email") String email,@Query("password") String password);
 }
